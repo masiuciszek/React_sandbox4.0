3d1cb3de1364bc155f09b0f3474f3e96c0b326ec
marcell <ciszekmarcell@gmail.com>
2019-06-24 20:46:56 +0200

    addin local sorage

--- jokes/src/components/Jokelist.js
+++ jokes/src/components/Jokelist.js
@@ -7,52 +7,82 @@ import Joke from './Joke';
 import './jokelist.css';
 
 function JokeList(props) {
-  const [jokes, setJokes] = useState([]);
+  const [jokes, setJokes] = useState(
+    JSON.parse(window.localStorage.getItem('jokes') || '[]')
+  );
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    getJokes();
+    if (jokes.length === 0) return getJokes();
   }, []);
 
   const getJokes = async () => {
     const joke = [];
+
     while (joke.length < props.numOfJokes) {
       const res = await axios.get('https://icanhazdadjoke.com/', {
         headers: { Accept: 'application/json' },
       });
       joke.push({ id: uuid(), text: res.data.joke, votes: 0 });
     }
-    setJokes(joke);
+    setJokes([...jokes, ...joke], () =>
+      window.localStorage.setItem('jokes', JSON.stringify(jokes))
+    );
+
+
+
+    setLoading(false);
+    window.localStorage.setItem('jokes', JSON.stringify(joke));
   };
 
   const handleVote = (id, delta) => {
     setJokes(
-      jokes.map(j => (j.id === id ? { ...j, votes: j.votes + delta } : j))
+      jokes.map(
+        j => (j.id === id ? { ...j, votes: j.votes + delta } : j),
+        () => window.localStorage.setItem('jokes', JSON.stringify(jokes))
+      )
     );
   };
+
+  const handleClick = () => {
+    setLoading(true, getJokes());
+  };
+  console.log(jokes);
+
+  const renderJokes = () =>
+    jokes.map(j => (
+      <Joke
+        key={j.id}
+        text={j.text}
+        vote={j.votes}
+        voteUp={() => handleVote(j.id, 1)}
+        voteDown={() => handleVote(j.id, -1)}
+      />
+    ));
 
   return (
     <div className="Jokelist">
       <div className="sidebar">
-        <h1>
+        <h1 className="jokelist-tilte">
           {' '}
           <span className="dad">Dad</span> Jokes{' '}
         </h1>
         <img src={logo} alt="smiley logo" />
-        <button type="button" className="add-joke-btn">
+        <button onClick={handleClick} type="button" className="add-joke-btn">
           Add Joke
         </button>
       </div>
 
       <div className="jokelist-jokes">
-        {jokes.map(j => (
-          <Joke
-            key={j.id}
-            text={j.text}
-            vote={j.votes}
-            voteUp={() => handleVote(j.id, 1)}
-            voteDown={() => handleVote(j.id, -1)}
-          />
-        ))}
+        {loading ? (
+          <div className="loading">
+            {' '}
+            <i className="far fa-8x fa-laugh fa-spin" />{' '}
+            <h1 className="jokelist-tilte">...Loading!!</h1>
+          </div>
+        ) : (
+          renderJokes()
+        )}
       </div>
     </div>
   );
